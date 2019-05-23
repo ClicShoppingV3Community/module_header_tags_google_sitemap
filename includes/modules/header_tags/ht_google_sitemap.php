c37fd6d0bd3cc2a191b0a2dcbd6da21675e7cc81
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:48 -0400

    reformat

--- includes/modules/header_tags/ht_google_sitemap.php
+++ includes/modules/header_tags/ht_google_sitemap.php
@@ -1,18 +1,19 @@
 <?php
-/**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   use ClicShopping\OM\Registry;
   use ClicShopping\OM\CLICSHOPPING;
 
-  class ht_google_sitemap {
+  class ht_google_sitemap
+  {
     public $code;
     public $group;
     public $title;
@@ -20,38 +21,43 @@
     public $sort_order;
     public $enabled = false;
 
-    public function __construct() {
+    public function __construct()
+    {
       $this->code = get_class($this);
       $this->group = basename(__DIR__);
 
       $this->title = CLICSHOPPING::getDef('module_header_tags_sitemap_title');
       $this->description = CLICSHOPPING::getDef('module_header_tags_sitemap_description');
 
-      if ( defined('MODULE_HEADER_TAGS_SITEMAP_STATUS') ) {
+      if (defined('MODULE_HEADER_TAGS_SITEMAP_STATUS')) {
         $this->sort_order = MODULE_HEADER_TAGS_SITEMAP_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_TAGS_SITEMAP_STATUS == 'True');
       }
     }
 
-    public function execute() {
+    public function execute()
+    {
 
       $CLICSHOPPING_Template = Registry::get('Template');
 
-       if (!is_null(MODULE_HEADER_TAGS_SITEMAP_ID)) {
+      if (!is_null(MODULE_HEADER_TAGS_SITEMAP_ID)) {
 
-        $CLICSHOPPING_Template->addBlock('<meta name="google-site-verification" content="'. MODULE_HEADER_TAGS_SITEMAP_ID .'" />', $this->group);
-       }
+        $CLICSHOPPING_Template->addBlock('<meta name="google-site-verification" content="' . MODULE_HEADER_TAGS_SITEMAP_ID . '" />', $this->group);
+      }
     }
 
-    public function isEnabled() {
+    public function isEnabled()
+    {
       return $this->enabled;
     }
 
-    public function check() {
+    public function check()
+    {
       return defined('MODULE_HEADER_TAGS_SITEMAP_STATUS');
     }
 
-    public function install() {
+    public function install()
+    {
       $CLICSHOPPING_Db = Registry::get('Db');
 
       $CLICSHOPPING_Db->save('configuration', [
@@ -90,19 +96,21 @@
         ]
       );
 
-       return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
-                                               ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
-                            );
+      return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
+        ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
+      );
     }
 
-    public function remove() {
+    public function remove()
+    {
       return Registry::get('Db')->exec('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
     }
 
-    public function keys() {
+    public function keys()
+    {
       return array('MODULE_HEADER_TAGS_SITEMAP_STATUS',
-                   'MODULE_HEADER_TAGS_SITEMAP_ID',
-                   'MODULE_HEADER_TAGS_SITEMAP_SORT_ORDER'
-                  );
+        'MODULE_HEADER_TAGS_SITEMAP_ID',
+        'MODULE_HEADER_TAGS_SITEMAP_SORT_ORDER'
+      );
     }
   }
